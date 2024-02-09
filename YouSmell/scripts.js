ef6ec5cea17f609fe88f19b48b6a2291b653a1f9
dylan-fournier <124598538+dylan-fournier@users.noreply.github.com>
2024-02-09 12:47:18 -0500

    fixed some stuff also cookies exist now

--- YouSmell/scripts.js
+++ YouSmell/scripts.js
@@ -1,7 +1,29 @@
 let smell = 0;
-function doFunction() {
-    document.getElementById("smellLine").innerHTML = "You are " + smell + "% smelly";
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+function smellIncrease() {
     smell = smell + 1;
+    document.getElementById("smellLine").innerHTML = "You are " + smell + "% smelly";
+    
+    document.cookie = "smell="+smell+"; path=/;";
 }
 
-document.getElementById("smellbutton").onclick = doFunction();
+console.log(getCookie("smell"));
+smell = Number(getCookie("smell"));
+
+document.getElementById("smellbutton").onclick = smellIncrease;
+
+document.getElementById("smellLine").innerHTML = "You are " + smell + "% smelly";
