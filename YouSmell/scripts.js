e3b94e2ffadd6384be0be602b5f1650c0ec4e7c9
dylan-fournier <dylan.fournier@stu.ocsb.ca>
2024-02-12 13:21:23 -0500

    new stuff and buttons and csslollz

--- YouSmell/scripts.js
+++ YouSmell/scripts.js
@@ -1,4 +1,5 @@
 let smell = 0;
+let clickpower = 1;
 function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
@@ -15,15 +16,30 @@ function getCookie(cname) {
     return "";
   }
 function smellIncrease() {
-    smell = smell + 1;
+    smell = smell + clickpower;
     document.getElementById("smellLine").innerHTML = "You are " + smell + "% smelly";
     
     document.cookie = "smell="+smell+"; path=/;";
 }
+function powerIncrease() {
+    if (smell>100) {
+      smell = smell - 100 
+      clickpower = clickpower+1
 
+      document.getElementById("smellLine").innerHTML = "You are " + smell + "% smelly";
+      document.getElementById("powerLine").innerHTML = "+"+clickpower+"% smell per click";
+      document.cookie = "clickpower="+clickpower+"; path=/;"
+    }
+}
+function test() {
+  document.getElementById("announcetext").innerHTML = "this works!"
+}
 console.log(getCookie("smell"));
 smell = Number(getCookie("smell"));
+clickpower= Number(getCookie("clickpower"))
 
 document.getElementById("smellbutton").onclick = smellIncrease;
-
+document.getElementById("powerbutton").onclick = powerIncrease;
+document.getElementById("savebutton").onclick = test
 document.getElementById("smellLine").innerHTML = "You are " + smell + "% smelly";
+document.getElementById("powerLine").innerHTML = "+"+clickpower+"% smell per click"
